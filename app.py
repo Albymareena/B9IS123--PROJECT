2d575fc15f09af64eb4dbafea4630830c623acdb
Albymareena Varghese <albymareenav@gmail.com>
2025-07-01 09:24:00 +0100

    Configure MySQL database with SQLAlchemy and initialize DB object

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from db import db  # added
+from flask_cors import CORS  # added
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@127.0.0.1:3306/milma_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
