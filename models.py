2cb4a865f527bfe219a56e7b5854526fb2d0ed87
Albymareena Varghese <albymareenav@gmail.com>
2025-07-09 07:08:00 +0100

    Add MilkCollection model for tracking milk production data

--- models.py
+++ models.py
@@ -10,3 +10,16 @@ class Farmer(db.Model):
 
     def to_dict(self):
         return dict(id=self.id, name=self.name, village=self.village, contact=self.contact, bank_account=self.bank_account)
+
+class MilkCollection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    farmer_id = db.Column(db.Integer, db.ForeignKey('farmer.id'), nullable=False)
+    date = db.Column(db.Date, default=datetime.utcnow)
+    volume_litres = db.Column(db.Float)
+    fat_percentage = db.Column(db.Float)
+    snf_percentage = db.Column(db.Float)
+
+    def to_dict(self):
+        return dict(id=self.id, farmer_id=self.farmer_id, date=self.date.isoformat(),
+                    volume_litres=self.volume_litres, fat_percentage=self.fat_percentage,
+                    snf_percentage=self.snf_percentage)
